c286c587b12d64608f55c1a2246a7a9ed5f8afcb
angelo59930 <manfrediangelosantiago10@gmail.com>
2021-10-03 16:51:43 -0300

    terminado la propuesta de soluciones parcial 1

--- modelo-parcial/propuesta1/Ej2/Ej2b.cpp
+++ modelo-parcial/propuesta1/Ej2/Ej2b.cpp
@@ -34,9 +34,7 @@ int main(void)
       for (int j = i + 1; j <= n - 1; j++)
       {
         if (fabs(m[j][i]) > errorMinimo) //Error parecido a 0.
-        {
-          for (int k = i; k <= n - 1; k++)
-          {
+          for (int k = i; k <= n - 1; k++){
             aux = m[i][k];
             m[i][k] = m[j][k];
             m[j][k] = aux;
@@ -48,8 +46,7 @@ int main(void)
           break;
         }
       }
-      if (cambio == 0)
-      {
+      if (cambio == 0){
         cout << "El Sistema es singular! no se puede resolver" << endl;
         return 0;
       }
@@ -57,8 +54,7 @@ int main(void)
         cout << "Se hizo pivoteo ! " << endl;
     }
     //***********************FIN PIVOTEO********************************
-    for (int j = i + 1; j <= n - 1; j++)
-    {
+    for (int j = i + 1; j <= n - 1; j++){
       f = (-m[j][i]) / (m[i][i]);
       for (int k = i; k <= n - 1; k++) //k arranca desde la diagonal principal.. k=i.
         m[j][k] = m[j][k] + f * m[i][k];
@@ -69,10 +65,8 @@ int main(void)
   //imprime la matriz como quedo!!
   cout << endl
        << "La Matriz triangular superior quedo: " << endl;
-  for (int i = 0; i < filas; i++)
-  {
-    for (int j = 0; j < columnas; j++)
-    {
+  for (int i = 0; i < filas; i++){
+    for (int j = 0; j < columnas; j++){
       cout << m[i][j] << " ";
     }
     cout << " ---> " << b[i];
@@ -86,11 +80,9 @@ int main(void)
   //valor de la ultima variable
   x[filas - 1] = b[filas - 1] / m[filas - 1][filas - 1];
 
-  for (int i = filas - 2; i >= 0; i--)
-  {
+  for (int i = filas - 2; i >= 0; i--){
     suma = b[i];
-    for (int j = i + 1; j <= n - 1; j++)
-    {
+    for (int j = i + 1; j <= n - 1; j++){
       suma -= m[i][j] * x[j];
     }
     x[i] = (suma) / m[i][i];
---
+++ modelo-parcial/propuesta1/Ej3/BiseRegula.cpp
@@ -0,0 +1,75 @@
+/*
+ * Estos metodos son buenos cuando la funcion f(x)
+ * a analizar se encuentra con el dominio acotado en [A,B]
+ * o si conocemos en que parte de la funcion se encuentra la raiz.
+ *
+ * ----------- DESVENTAJAS/A TENER EN CUENTA ------------
+ *   . Solo puede encontrar UNA SOLA raiz.
+ *   . Estamos obligados a que en el intervalo [A,B]
+ *     la funcion pase por el cero, es decir, 
+ *     f(A) > 0 y f(B) < 0 o al revez.
+ *  -----------------------------------------------------
+ * 
+ * */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+* Estas dos funciones en "esencia" hacen lo mismo
+* con la diferencia que NORMALMENTE
+* el metodo de Regular Falsi("puntoMedioR) es mas rapido
+* */
+double puntoMedioR(double a, double b);
+double puntoMedioB(double a, double b);
+
+// Aca escribiremos la funcion de la cual buscaremos las raices
+double f(double x);
+
+int main(int argc, char const *argv[]){
+  double a = 0.8,b = 1.3,c,error = 1,errorMin = pow(10,-8),anterior;
+  
+ 
+  //Con esta condicion nos aseguramos de que el rango A y B no esten del mismo lado
+  if(f(a)*f(b)>0){
+    cout << "no hay raices en el intervalo o tiene raices pares" << endl;
+    return 0;
+  }
+  // esta variabla nos ayudara a calcular el error aproximado
+  anterior = 0;
+  while(error > errorMin){
+  
+    /*
+     * A partir de aca es donde comienza realmente el metodo para encontrar raices.
+     * Como en lo que se diferencia cada metodo es como calcula el valor del punto medio,
+     * unicamente cambiando la fucion que lo calcula ya estariamos cambiado de metodo.
+     * */
+    c = puntoMedioB(a, b);
+    if(f(a)*f(c)>0)
+      a=c;
+    else if(f(b)*f(c)>0)
+      b=c;
+    else
+      break;
+
+    // Calculamos el error aproximado siende este el valor absoluto del punto "c" con el anterior
+    error = fabs(c-anterior);
+    anterior = c; 
+  }
+
+  printf("el valor de la raiz es:%f",c);
+  return 0;
+}
+
+double f(double x){
+  return (-15.3315 + 58.6036*x + -74.2202*x*x + 30.9504*x*x*x);
+}
+
+// Calculo de punto medio del met. Regula falsi
+double puntoMedioR(double a, double b){ return b-((f(b)*(a-b))/(f(a)-f(b))); }
+
+// Calculo de punto medio del met. biseccion.
+double puntoMedioB(double a, double b){ return (b+a)/2; }
+
